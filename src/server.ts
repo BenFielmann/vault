c0051c4c35be4d944aaeae024f3514a4212e23b4
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-10 23:13:23 +0200

    🚧  Not done yet

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import { readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
+
+app.get('/api/credentials', async (_req, res) => {
+  res.status(200).json(await readCredentials());
+});
+
+/*app.get('/api/credential/:service' async (request, response) => {
+
+});*/
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
