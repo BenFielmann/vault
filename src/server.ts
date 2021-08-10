1e1e8ab8ab77962521609f1d374c6f5fc3555ce8
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-10 16:36:04 +0200

    ✨ Creater basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
-import { readCredentials } from './utils/credentials';
+import express from 'express';
 
-console.log(readCredentials());
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
