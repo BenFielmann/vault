cba860387a7862ddf93b46abb4b4d0919ee99b9b
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-11 18:05:15 +0200

    Add add, delete, overWrite function

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { DB, Credential } from '../types';
 
 export async function readCredentials(): Promise<Credential[]> {
@@ -19,3 +19,24 @@ export const findCredential = async (service: string): Promise<Credential> => {
 
   return credential;
 };
+
+export const deleteCredential = async (service: string): Promise<void> => {
+  const credentials = await readCredentials();
+  const filteredCredential = credentials.filter(
+    (credential) => credential.service.toLowerCase() !== service.toLowerCase()
+  );
+  const db: DB = { credentials: filteredCredential };
+  await overWriteDB(db);
+};
+
+export async function addCredential(credential: Credential): Promise<void> {
+  const response = await readFile('src/db.json', 'utf-8');
+  const db: DB = JSON.parse(response);
+  db.credentials = [...db.credentials, credential];
+  await overWriteDB(db);
+}
+
+async function overWriteDB(db: DB): Promise<void> {
+  const dbString = JSON.stringify(db, null, 2);
+  await writeFile('src/db.json', dbString);
+}
