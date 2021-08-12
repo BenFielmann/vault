42c2bd0eb374be4a100e46454ae5e6dca6f7746e
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-12 17:09:16 +0200

    ✨ Add export encryptCredentials

---
+++ src/utils/crypto.ts
@@ -0,0 +1,32 @@
+import CryptoJS from 'crypto-js';
+import { Credential } from '../types';
+
+const masterKey = 'Zippo';
+
+export const encryptCredential = (credential: Credential): Credential => {
+  const encryptedPassword = CryptoJS.TripleDES.encrypt(
+    credential.password,
+    masterKey
+  ).toString();
+
+  const encryptedCredential = {
+    ...credential,
+    password: encryptedPassword,
+  };
+
+  return encryptedCredential;
+};
+
+export const decryptCredential = (credential: Credential): Credential => {
+  const decryptedPassword = CryptoJS.TripleDES.decrypt(
+    credential.password,
+    masterKey
+  ).toString(CryptoJS.enc.Utf8);
+
+  const decryptedCredential = {
+    ...credential,
+    password: decryptedPassword,
+  };
+
+  return decryptedCredential;
+};
