6e958a725d1739fcf9b0ae75c86b84615b0d0f33
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-12 16:21:07 +0200

    ✨ Add funktion to encrypt password

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,3 +1,4 @@
+import CryptoJS from 'crypto-js';
 import { readFile, writeFile } from 'fs/promises';
 import { DB, Credential } from '../types';
 
@@ -19,6 +20,30 @@ export const findCredential = async (service: string): Promise<Credential> => {
 
   return credential;
 };
+
+// partiallyEncryptedCredential = { everything in credential, encrypted password }
+// add partiallyEncryptedCredential to newCredentials in line below
+
+export async function addCredential(credential: Credential): Promise<void> {
+  const response = await readFile('src/db.json', 'utf-8');
+  const encrypted = CryptoJS.TripleDES.encrypt(credential.password, 'Zippo');
+  credential.password = encrypted.toString();
+  const db: DB = JSON.parse(response);
+  db.credentials = [...db.credentials, credential];
+  await overWriteDB(db);
+}
+
+/*export async function addCredential(credential: Credential): Promise<void> {
+  const credentials = await readCredentials();
+  const encrypted = CryptoJS.TripleDES.encrypt(credential.password, 'Zippo');
+  credential.password = encrypted.toString();
+  const newCredentials = [...credentials, credential];
+  const newDB: DB = {
+    credentials: newCredentials,
+    //const decrypted = CryptoJS.TripleDES.decrypt(encrypted, "Zippo");
+  };
+  await writeFile('src/db.json', JSON.stringify(newDB, null, 2));
+}*/
 
 export const deleteCredential = async (service: string): Promise<void> => {
   const credentials = await readCredentials();
@@ -46,17 +71,6 @@ export const updateCredential = async (
   };
   return await overWriteDB(db);
 };
-
-//get all credentials
-//modify by one
-//overwrite DB
-
-export async function addCredential(credential: Credential): Promise<void> {
-  const response = await readFile('src/db.json', 'utf-8');
-  const db: DB = JSON.parse(response);
-  db.credentials = [...db.credentials, credential];
-  await overWriteDB(db);
-}
 
 async function overWriteDB(db: DB): Promise<void> {
   const dbString = JSON.stringify(db, null, 2);
