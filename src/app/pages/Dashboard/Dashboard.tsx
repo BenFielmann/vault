5938b8ba0a63691b6ff124622aa3b8470dc7809f
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-19 15:31:36 +0200

    ✨ Add password.tsx, set basic route and link

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Dashboard.module.css';
 import logo from './logo.svg';
 
 export default function Dashboard(): JSX.Element {
   return (
     <div className={styles.container}>
-      <img src={logo} className={styles['App-logo']} alt="logo" />
       <h1 className={styles.headline}>Password Vault</h1>
+      <img src={logo} className={styles['App-logo']} alt="logo" />
       <input className={styles.inputField} placeholder="Password" />
+      <Link to="/vault">Vault</Link>
     </div>
   );
 }
