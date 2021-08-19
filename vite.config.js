6a1863557ce40086eec1d21909f1bd75930f1e5c
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-19 15:43:01 +0200

    🔧 Update vite.config to use Proxy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import dotenv from 'dotenv';
+dotenv.config();
+const { PORT = 3001 } = process.env;
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:${PORT}',
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist/app',
   },
