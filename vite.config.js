1eb3c52dc66d4ad008e827b272045535ce528c05
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-19 16:22:51 +0200

    🚑 Fix vite proxy error

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:${PORT}',
+        target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
     },
