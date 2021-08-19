da65fa0eeaea673755f89d495b1957ab3665a1ee
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-19 16:15:05 +0200

    ✨ Implement use effect to dashboard

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Dashboard.module.css';
 import logo from './logo.svg';
 
 export default function Dashboard(): JSX.Element {
+  const [credentials, setCredentials] = useState([]);
+
+  useEffect(() => {
+    async function fetchCredentials() {
+      const response = await fetch('/api/credentials', {
+        headers: {
+          Authorization: 'Zippo',
+        },
+      });
+      const credentials = await response.json();
+      setCredentials(credentials);
+    }
+    fetchCredentials();
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <h1 className={styles.headline}>Password Vault</h1>
       <img src={logo} className={styles['App-logo']} alt="logo" />
-      <input className={styles.inputField} placeholder="Password" />
       <Link to="/vault">Vault</Link>
-    </div>
+      <input className={styles.inputField} placeholder="Password" />
+      {credentials &&
+        credentials.forEach((Credential) => console.log(Credential))}
+    </main>
   );
 }
