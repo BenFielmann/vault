805fb7235ad22f8764721d1c8ed838d789e0f8b6
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-08-23 14:53:47 +0200

    ✨ Add route to search, add, edit-credential

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Password from './pages/Password/Password';
-import AddPage from './pages/AddPage/AddPage';
+import AddPage from './pages/AddPage/Add';
+import Search from './pages/Search/Search';
+import Edit from './pages/Edit/Edit';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/add">
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/credential/add">
           <AddPage />
+        </Route>
+        <Route path="/credential/:service/edit">
+          <Edit />
         </Route>
         <Route path="/password/:service">
           <Password />
